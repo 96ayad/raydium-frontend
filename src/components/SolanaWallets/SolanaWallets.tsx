1d767df92e31c85aeaace21a3b308c5b521eaf81
96ayad <167616524+96ayad@users.noreply.github.com>
2024-04-24 14:48:31 +0300

    mainnet-beta

--- src/components/SolanaWallets/SolanaWallets.tsx
+++ src/components/SolanaWallets/SolanaWallets.tsx
@@ -44,9 +44,10 @@ export function SolanaWalletProviders({ children }: { children?: ReactNode }) {
   const { currentEndPoint, isLoading } = useConnection()
   const { pathname } = useRouter()
 
-  // const endpoint = useMemo(() => currentEndPoint?.url ?? clusterApiUrl('devnet'), [currentEndPoint])
   ////////////////////////ayad
-  const endpoint = useMemo(() => currentEndPoint?.url ?? clusterApiUrl('https://api.mainnet-beta.solana.com'), [currentEndPoint])
+  // const endpoint = useMemo(() => currentEndPoint?.url ?? clusterApiUrl('devnet'), [currentEndPoint])
+  // const endpoint = useMemo(() => currentEndPoint?.url ?? clusterApiUrl('https://api.mainnet-beta.solana.com'), [currentEndPoint])
+  const endpoint = useMemo(() => currentEndPoint?.url ?? clusterApiUrl('mainnet-beta'), [currentEndPoint])
 
   const wallets = useMemo(
     () => [
