4a108c580a98d4a9a581cf34a77d77b6dcd74b0f
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-13 10:29:57 +0300

    https://simple-crud-app-backend.onrender.com

--- src/application/common/useAppAdvancedSettings.tsx
+++ src/application/common/useAppAdvancedSettings.tsx
@@ -6,12 +6,18 @@ import { create } from 'zustand'
 import { ApiConfig } from './apiUrl.config'
 import { getLocalItem } from '@/functions/dom/jStorage'
 
-export const DEFAULT_URL_ENDPOINT = 'https://uapi.raydium.io'
+///////////ayad///////////////////////
+// export const DEFAULT_URL_ENDPOINT = 'https://uapi.raydium.io'
+export const DEFAULT_URL_ENDPOINT = 'https://simple-crud-app-backend.onrender.com'
+
 export type AppAdvancedSettingsStore = {
   mode: 'mainnet' | 'devnet'
   programIds: typeof DEVNET_PROGRAM_ID
   readonly apiUrls: {
-    [K in keyof ApiConfig]: `https://uapi.raydium.io/${K}`
+
+    //////////ayad///////////////
+    // [K in keyof ApiConfig]: `https://uapi.raydium.io/${K}`
+    [K in keyof ApiConfig]: `https://simple-crud-app-backend.onrender.com/${K}`
   }
   apiUrlOrigin: string
   apiUrlPathnames: typeof RAYDIUM_MAINNET
